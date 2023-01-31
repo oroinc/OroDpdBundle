978161c65cfd754c51a5c018a62081afe015eafa
Vova Soroka <vsoroka@oroinc.com>
2023-01-31 18:02:28 +0200

    BB-21884: Refactor shipping method factories (#34758)
    - add strict types to Oro\Bundle\ShippingBundle\Method\Factory\IntegrationShippingMethodFactoryInterface and all classes that implement this interface
    - simplify code of all classes that implement Oro\Bundle\ShippingBundle\Method\Factory\IntegrationShippingMethodFactoryInterface
    - add strict types to Oro\Bundle\IntegrationBundle\Generator\IntegrationIdentifierGeneratorInterface and all classes that implement this interface

--- Method/Factory/DPDShippingMethodFactory.php
+++ Method/Factory/DPDShippingMethodFactory.php
@@ -3,40 +3,24 @@
 namespace Oro\Bundle\DPDBundle\Method\Factory;
 
 use Oro\Bundle\DPDBundle\Entity\DPDTransport as DPDSettings;
-use Oro\Bundle\DPDBundle\Entity\ShippingService;
 use Oro\Bundle\DPDBundle\Method\DPDShippingMethod;
 use Oro\Bundle\IntegrationBundle\Entity\Channel;
 use Oro\Bundle\IntegrationBundle\Generator\IntegrationIdentifierGeneratorInterface;
 use Oro\Bundle\IntegrationBundle\Provider\IntegrationIconProviderInterface;
 use Oro\Bundle\LocaleBundle\Helper\LocalizationHelper;
 use Oro\Bundle\ShippingBundle\Method\Factory\IntegrationShippingMethodFactoryInterface;
+use Oro\Bundle\ShippingBundle\Method\ShippingMethodInterface;
 
+/**
+ * The factory to create DPD shipping method.
+ */
 class DPDShippingMethodFactory implements IntegrationShippingMethodFactoryInterface
 {
-    /**
-     * @var LocalizationHelper
-     */
-    private $localizationHelper;
-
-    /**
-     * @var IntegrationIdentifierGeneratorInterface
-     */
-    private $methodIdentifierGenerator;
-
-    /**
-     * @var DPDShippingMethodTypeFactoryInterface
-     */
-    private $methodTypeFactory;
-
-    /**
-     * @var DPDHandlerFactoryInterface
-     */
-    private $handlerFactory;
-
-    /**
-     * @var IntegrationIconProviderInterface
-     */
-    private $integrationIconProvider;
+    private LocalizationHelper $localizationHelper;
+    private IntegrationIdentifierGeneratorInterface $methodIdentifierGenerator;
+    private DPDShippingMethodTypeFactoryInterface $methodTypeFactory;
+    private DPDHandlerFactoryInterface $handlerFactory;
+    private IntegrationIconProviderInterface $integrationIconProvider;
 
     public function __construct(
         LocalizationHelper $localizationHelper,
@@ -53,87 +37,27 @@ class DPDShippingMethodFactory implements IntegrationShippingMethodFactoryInterf
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    public function create(Channel $channel)
+    public function create(Channel $channel): ShippingMethodInterface
     {
+        /** @var DPDSettings $transport */
+        $transport = $channel->getTransport();
+        $types = [];
+        $handlers = [];
+        $applicableShippingServices = $transport->getApplicableShippingServices()->toArray();
+        foreach ($applicableShippingServices as $shippingService) {
+            $types[] = $this->methodTypeFactory->create($channel, $shippingService);
+            $handlers[] = $this->handlerFactory->create($channel, $shippingService);
+        }
+
         return new DPDShippingMethod(
-            $this->getIdentifier($channel),
-            $this->getLabel($channel),
+            $this->methodIdentifierGenerator->generateIdentifier($channel),
+            (string)$this->localizationHelper->getLocalizedValue($transport->getLabels()),
             $channel->isEnabled(),
-            $this->getIcon($channel),
-            $this->createTypes($channel),
-            $this->createHandlers($channel)
+            $this->integrationIconProvider->getIcon($channel),
+            $types,
+            $handlers
         );
-    }
-
-    /**
-     * @param Channel $channel
-     *
-     * @return string
-     */
-    private function getIdentifier(Channel $channel)
-    {
-        return $this->methodIdentifierGenerator->generateIdentifier($channel);
-    }
-
-    /**
-     * @param Channel $channel
-     *
-     * @return string
-     */
-    private function getLabel(Channel $channel)
-    {
-        $settings = $this->getSettings($channel);
-
-        return (string) $this->localizationHelper->getLocalizedValue($settings->getLabels());
-    }
-
-    /**
-     * @param Channel $channel
-     *
-     * @return \Oro\Bundle\IntegrationBundle\Entity\Transport|DPDSettings
-     */
-    private function getSettings(Channel $channel)
-    {
-        return $channel->getTransport();
-    }
-
-    /**
-     * @param Channel $channel
-     *
-     * @return array
-     */
-    private function createTypes(Channel $channel)
-    {
-        $applicableShippingServices = $this->getSettings($channel)->getApplicableShippingServices()->toArray();
-
-        return array_map(function (ShippingService $shippingService) use ($channel) {
-            return $this->methodTypeFactory->create($channel, $shippingService);
-        }, $applicableShippingServices);
-    }
-
-    /**
-     * @param Channel $channel
-     *
-     * @return array
-     */
-    private function createHandlers(Channel $channel)
-    {
-        $applicableShippingServices = $this->getSettings($channel)->getApplicableShippingServices()->toArray();
-
-        return array_map(function (ShippingService $shippingService) use ($channel) {
-            return $this->handlerFactory->create($channel, $shippingService);
-        }, $applicableShippingServices);
-    }
-
-    /**
-     * @param Channel $channel
-     *
-     * @return string|null
-     */
-    private function getIcon(Channel $channel)
-    {
-        return $this->integrationIconProvider->getIcon($channel);
     }
 }
