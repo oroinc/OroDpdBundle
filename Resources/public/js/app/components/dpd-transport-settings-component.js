5084eb2c8c74812e7a4f65fc539a5a66e1093f7e
Alex Yegerev <1755578+lexxcode@users.noreply.github.com>
2025-10-15 18:01:40 +0400

    BB-26262: Convert for dotmailer, google-tag-manager, infinitepay, dpd, ldap, mailchimp (#42717)
    * BB-26262: Convert for dotmailer, google-tag-manager, infinitepay, dpd, ldap, mailchimp

--- Resources/public/js/app/components/dpd-transport-settings-component.js
+++ Resources/public/js/app/components/dpd-transport-settings-component.js
@@ -1,69 +1,65 @@
-define(function(require) {
-    'use strict';
+import $ from 'jquery';
+import _ from 'underscore';
+import LoadingMaskView from 'oroui/js/app/views/loading-mask-view';
+import BaseComponent from 'oroui/js/app/components/base/component';
 
-    const $ = require('jquery');
-    const _ = require('underscore');
-    const LoadingMaskView = require('oroui/js/app/views/loading-mask-view');
-    const BaseComponent = require('oroui/js/app/components/base/component');
+const DPDTransportSettingsComponent = BaseComponent.extend({
+    /**
+     * @property {Object}
+     */
+    options: {
+        ratePolicySelector: 'select[name$="[transport][ratePolicy]"]',
+        flatRatePriceValueSelector: 'input[name$="[transport][flatRatePriceValue]"]',
+        ratesCsvSelector: 'input[name$="[transport][ratesCsv]"]',
+        container: '.control-group'
+    },
 
-    const DPDTransportSettingsComponent = BaseComponent.extend({
-        /**
-         * @property {Object}
-         */
-        options: {
-            ratePolicySelector: 'select[name$="[transport][ratePolicy]"]',
-            flatRatePriceValueSelector: 'input[name$="[transport][flatRatePriceValue]"]',
-            ratesCsvSelector: 'input[name$="[transport][ratesCsv]"]',
-            container: '.control-group'
-        },
+    /**
+     * @inheritdoc
+     */
+    constructor: function DPDTransportSettingsComponent(options) {
+        DPDTransportSettingsComponent.__super__.constructor.call(this, options);
+    },
 
-        /**
-         * @inheritdoc
-         */
-        constructor: function DPDTransportSettingsComponent(options) {
-            DPDTransportSettingsComponent.__super__.constructor.call(this, options);
-        },
+    /**
+     * @inheritdoc
+     */
+    initialize: function(options) {
+        this.options = _.defaults(options || {}, this.options);
+        this.$elem = options._sourceElement;
 
-        /**
-         * @inheritdoc
-         */
-        initialize: function(options) {
-            this.options = _.defaults(options || {}, this.options);
-            this.$elem = options._sourceElement;
+        this.loadingMaskView = new LoadingMaskView({container: this.$elem});
+        this.ratePolicyElem = $(this.$elem).find(this.options.ratePolicySelector);
+        this.flatRatePriceValueElem = $(this.$elem).find(this.options.flatRatePriceValueSelector);
+        this.ratesCsvElem = $(this.$elem).find(this.options.ratesCsvSelector);
 
-            this.loadingMaskView = new LoadingMaskView({container: this.$elem});
-            this.ratePolicyElem = $(this.$elem).find(this.options.ratePolicySelector);
-            this.flatRatePriceValueElem = $(this.$elem).find(this.options.flatRatePriceValueSelector);
-            this.ratesCsvElem = $(this.$elem).find(this.options.ratesCsvSelector);
+        $(this.ratePolicyElem).on('change', this.onRatePolicyChange.bind(this));
+        $(this.ratePolicyElem).trigger('change');
+    },
 
-            $(this.ratePolicyElem).on('change', this.onRatePolicyChange.bind(this));
-            $(this.ratePolicyElem).trigger('change');
-        },
+    onRatePolicyChange: function() {
+        const ratePolicyValue = $(this.ratePolicyElem).val();
+        const self = this;
 
-        onRatePolicyChange: function() {
-            const ratePolicyValue = $(this.ratePolicyElem).val();
-            const self = this;
-
-            if (ratePolicyValue === '0') { // DPDTransport::FLAT_RATE_POLICY
-                $(this.flatRatePriceValueElem).closest(self.options.container).show();
-                $(this.ratesCsvElem).closest(self.options.container).hide();
-            } else if (ratePolicyValue === '1') { // DPDTransport::TABLE_RATE_POLICY
-                $(this.flatRatePriceValueElem).closest(self.options.container).hide();
-                $(this.ratesCsvElem).closest(self.options.container).show();
-            }
-        },
-
-        dispose: function() {
-            if (this.disposed) {
-                return;
-            }
-
-            this.$elem.off();
-            this.$elem.find(this.options.countrySelector).off();
-
-            DPDTransportSettingsComponent.__super__.dispose.call(this);
+        if (ratePolicyValue === '0') { // DPDTransport::FLAT_RATE_POLICY
+            $(this.flatRatePriceValueElem).closest(self.options.container).show();
+            $(this.ratesCsvElem).closest(self.options.container).hide();
+        } else if (ratePolicyValue === '1') { // DPDTransport::TABLE_RATE_POLICY
+            $(this.flatRatePriceValueElem).closest(self.options.container).hide();
+            $(this.ratesCsvElem).closest(self.options.container).show();
         }
-    });
+    },
 
-    return DPDTransportSettingsComponent;
+    dispose: function() {
+        if (this.disposed) {
+            return;
+        }
+
+        this.$elem.off();
+        this.$elem.find(this.options.countrySelector).off();
+
+        DPDTransportSettingsComponent.__super__.dispose.call(this);
+    }
 });
+
+export default DPDTransportSettingsComponent;
